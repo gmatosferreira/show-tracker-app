ae28c9620a855ae1536d350bc2e2b93ca865bf65
Gonçalo Matos <gmatos.ferreira@ua.pt>
2020-05-31 00:49:23 +0100

    removed API and implemented local db for series info

--- viewmodel/js/search.js
+++ viewmodel/js/search.js
@@ -4,16 +4,35 @@ $(document).ready(function(){
     params = (new URL(window.location)).searchParams;
     keyword = params.get('q');
 
-    // Validate keyword
-    if (keyword=="" || keyword==null) {
-        window.location.replace("index.html");
-    }
-
     // Search
-    baseUri = 'https://tvdbapiproxy.leonekmi.fr/search/series?name='+keyword;
-    ajaxHelper(baseUri, 'GET').done(function (data) {
-        alert("Done");
-        console.log(data);
+    $("#searchZeroResults").hide();
+    $("#searchField").val(keyword);
+    $("#searchLabel").text(keyword);
+
+    results = []
+    $.getJSON("https://ihc.gmatos.pt/DB/series.json", function(json) {
+        json['data'].forEach((value, index) => {
+            if (keyword=="" || keyword==null) {
+                results.push(value)   
+            } else if (value['seriesName'].toLowerCase().includes(keyword.toLowerCase())) {
+                results.push(value)   
+            }
+        });
+
+        results.forEach((value, index) => {
+            status = ""
+            if(value['status']=='Continuing')
+                status = "Em exibição"
+            else if(value['status']=='Ended')
+            status = "Terminada"
+            if (value['overview']==undefined)
+                value['overview'] = ""
+            $("#searchResults").append(`<article class="style1"> <span class="image"> <img src="https://www.thetvdb.com${value['poster']}" alt="" /> </span> <a href="episode.html?id=${value['id']}"><h2>${value['seriesName']}</h2><div class="content"><p>${status}</p><p>${value['overview']}</p></div> </a> </article>`);            
+        });
+
+        if(results.length==0) {
+            $("#searchZeroResults").fadeIn();
+        }
     });
 
 
