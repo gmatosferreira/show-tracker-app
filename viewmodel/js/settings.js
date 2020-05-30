1b5ff02b8c767ffef36b40b66bd8d3b2f896049b
Francisca Barros <kikas4323@gmail.com>
2020-05-30 17:49:21 +0100

    ines changed settings

--- viewmodel/js/settings.js
+++ viewmodel/js/settings.js
@@ -16,5 +16,36 @@ $(document).ready(function(){
         $("#moreNotifications").text("Show ");
     });
 
+    $('#collapsePrivacy').on('show.bs.collapse', function () {
+        $("#morePrivacy").text("Hide ");
+    });
 
+    $('#collapsePrivacy').on('hide.bs.collapse', function () {
+        $("#morePrivacy").text("Show ");
+    });
+
+    $('#collapseSecurity').on('show.bs.collapse', function () {
+        $("#moreSecurity").text("Hide ");
+    });
+
+    $('#collapseSecurity').on('hide.bs.collapse', function () {
+        $("#moreSecurity").text("Show ");
+    });
+
+    $('#collapseHelp').on('show.bs.collapse', function () {
+        $("#moreHelp").text("Hide ");
+    });
+
+    $('#collapseHelp').on('hide.bs.collapse', function () {
+        $("#moreHelp").text("Show ");
+    });
+
+    $('#collapseAbout').on('show.bs.collapse', function () {
+        $("#moreAbout").text("Hide ");
+    });
+
+    $('#collapseAbout').on('hide.bs.collapse', function () {
+        $("#moreAbout").text("Show ");
+    });
+    
 });
