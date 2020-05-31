e6f85b94f62ca3115aed88c7aefdce7810792380
Gonçalo Matos <gmatos.ferreira@ua.pt>
2020-05-31 22:08:20 +0100

    implemented navigation between episodes on episode.html

--- viewmodel/js/episode.js
+++ viewmodel/js/episode.js
@@ -13,6 +13,8 @@ $(document).ready(function () {
     // Get episodes data
     episodes = null;
     episodeInfo = null;
+    prevEpisode = null;
+    nextEpisode = null;
     $.getJSON("https://ihc.gmatos.pt/DB/seriesDetails.json", function (json) {
         json.forEach((value, index) => {
             if (value['id'] == series) {
@@ -21,15 +23,34 @@ $(document).ready(function () {
             }
         });
 
+        // Sort episodes by season and episode
+        episodes.sort(function (a, b) { return a['airedSeason'] == b['airedSeason'] ? a['airedEpisodeNumber'] - b['airedEpisodeNumber'] : a['airedSeason'] - b['airedSeason'] });
+        console.log(episodes);
+
         if (episodes == null) {
             window.history.back();
         }
 
         // Get episode from episodes list
         episodes.forEach((value, index) => {
-            if (value['airedSeason']==season && value['airedEpisodeNumber']==episode) {
+            if (value['airedSeason'] == season && value['airedEpisodeNumber'] == episode) {
                 episodeInfo = value;
                 console.log(episodeInfo);
+                // Check if has next and prev
+                if (index != 0) {
+                    console.log("hasPrevEpisode");
+                    prevEpisode = {
+                        'season': episodes[index - 1]['airedSeason'],
+                        'episode': episodes[index - 1]['airedEpisodeNumber'],
+                    };
+                }
+                if (index < episodes.length - 1) {
+                    console.log("hasNextEpisode");
+                    nextEpisode = {
+                        'season': episodes[index + 1]['airedSeason'],
+                        'episode': episodes[index + 1]['airedEpisodeNumber'],
+                    };
+                }
                 return;
             }
         });
@@ -38,43 +59,67 @@ $(document).ready(function () {
             window.history.back();
         }
 
-        if (episodeInfo['overview']=="" || episodeInfo['overview']==undefined || episodeInfo['overview']==null)
+        if (episodeInfo['overview'] == "" || episodeInfo['overview'] == undefined || episodeInfo['overview'] == null)
             episodeInfo['overview'] = "Episode's overview is not available :("
 
-        if (episodeInfo['filename']=="" || episodeInfo['filename']==undefined || episodeInfo['filename']==null)
+        if (episodeInfo['filename'] == "" || episodeInfo['filename'] == undefined || episodeInfo['filename'] == null)
             episodeInfo['filename'] = 'https://ihc.gmatos.pt/images/notavailableEpisode.jpg'
         else
-            episodeInfo['filename'] = 'https://thetvdb.com/banners/'+episodeInfo['filename']
+            episodeInfo['filename'] = 'https://thetvdb.com/banners/' + episodeInfo['filename']
 
-        if(!userWatchingSeriesByEpisode(episodeInfo)) {
+        if (!userWatchingSeriesByEpisode(episodeInfo)) {
             $("#watched").attr('disabled', true);
             $("#watched").text("Add this series to your watchlist to mark this episode as seen");
         }
-            
+
 
         // Add data to HTML
         $("#episodeInfo h1").text(`S. ${episodeInfo['airedSeason']} Ep. ${episodeInfo['airedEpisodeNumber']} | ${episodeInfo['episodeName']}`);
         $("#episodeInfo img").attr('src', episodeInfo['filename']);
         $("#episodeInfo p").text(episodeInfo['overview']);
-        $("#episodeInfo a").attr('href', 'series.html?id='+episodeInfo['seriesId'])
-        if(episodeWatched(episodeInfo)) {
+        $("#episodeInfo #seriesPage").attr('href', 'series.html?id=' + episodeInfo['seriesId'])
+        if (episodeWatched(episodeInfo)) {
             $("#watched").attr('disabled', true);
             $("#watched").text("Watched");
         }
-        
+
         // Show HTML
         $("#episodeInfo").removeClass("d-none");
         $("#episodeInfo").hide();
         $("#episodeInfo").fadeIn();
 
+        console.log("Prev and next episodes");
+        console.log(prevEpisode);
+        console.log(nextEpisode);
+
+        if (prevEpisode == null) {
+            $("#prevEpisode").attr('disabled', true);
+        }
+        if (nextEpisode == null) {
+            $("#nextEpisode").attr('disabled', true);
+        }
+
     });
 
     // Mark episode as watched
-    $("#watched").click(function(){      
-        success = markEpisodeWatched(episodeInfo);  
-        console.log("Return status: "+success);
+    $("#watched").click(function () {
+        success = markEpisodeWatched(episodeInfo);
+        console.log("Return status: " + success);
         window.location.reload();
         console.log(userLogin);
+    });
+
+    // Go to next and prev episode
+    $("#prevEpisode").click(function(){
+        if (prevEpisode!=null) {
+            window.location.replace(`episode.html?series=${episodeInfo['seriesId']}&se=${prevEpisode['season']}&ep=${prevEpisode['episode']}`)
+        }
+    });
+
+    $("#nextEpisode").click(function(){
+        if (nextEpisode!=null) {
+            window.location.replace(`episode.html?series=${episodeInfo['seriesId']}&se=${nextEpisode['season']}&ep=${nextEpisode['episode']}`)
+        }
     });
 
 });
