4af6716cb37cb3a6f7617235f7edcde8d9529def
Gonçalo Matos <gmatos.ferreira@ua.pt>
2020-05-31 01:54:20 +0100

    added episodes data to series.html

--- viewmodel/js/series.js
+++ viewmodel/js/series.js
@@ -20,12 +20,43 @@ $(document).ready(function(){
         if (series==null)
             window.history.back();
 
-        $("#main h1").text(series['seriesName']);
-        $("#main img").attr('src', 'https://www.thetvdb.com'+series['banner']);
+        $("#episodeData h1").text(series['seriesName']);
+        $("#episodeData img").attr('src', 'https://www.thetvdb.com'+series['banner']);
         if (series['overview']==null)
             series['overview'] = "Sinopse não disponível";
-        $("#main p").text(series['overview']);
+        $("#episodeData p").text(series['overview']);
         $("#main").removeClass("d-none");
+    });
+
+    // Get episodes data
+    episodes = null;
+    $.getJSON("https://ihc.gmatos.pt/DB/seriesDetails.json", function(json) {
+        json.forEach((value, index) => {
+            if (value['id']==id) {
+                episodes = value['episodes'];
+                console.log(episodes);
+            }
+        });
+
+        if (episodes==null) {
+            $("episodesError").removeClass("d-none");
+            return;
+        } 
+
+        season = 0;
+        html = ""
+        episodes.forEach((value, index) => {
+            if(value['airedSeason']>season) {
+                if(season!=0)
+                    html += `</div></section>`;
+                season = value['airedSeason'];
+                html += `<h2 class="small mt-5">Season ${season}</h2>`;
+                html += `<section class="card"><div class="card-body row mx-0">`;
+            }
+            html += `<div class="col-4 p-4"><a href="episode.html?series=${id}&se=${season}&ep=${value['airedEpisodeNumber']}"><img class="w-100" src="https://www.thetvdb.com/banners/${value['filename']}" alt=""></a><a href="episode.html?series=${id}&se=${season}&ep=${value['airedEpisodeNumber']}"><h2 class="m-0 text-center">Episode ${value['airedEpisodeNumber']}</h2></a><p class="small text-center">Aired on ${value['firstAired']}</p></div>`;
+        });      
+        
+        $("#episodes").append(html);
     });
 
 
